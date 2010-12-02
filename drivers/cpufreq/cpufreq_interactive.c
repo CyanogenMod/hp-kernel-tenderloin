ffa113126489dda6f22ab6fe1eb085a6febc3522
Mike Chan <mike@android.com>
2010-12-01 19:06:50 -0500

    cpufreq: interactive: New 'interactive' governor
    (Sync with final version from Google)
    
    http://android.git.kernel.org/?p=kernel/common.git;a=commit;h=5316236c8da6bf86a15f9e90105e67de4df95200

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -53,15 +53,19 @@ static struct task_struct *up_task;
 static struct workqueue_struct *down_wq;
 static struct work_struct freq_scale_down_work;
 static cpumask_t up_cpumask;
+static spinlock_t up_cpumask_lock;
 static cpumask_t down_cpumask;
+static spinlock_t down_cpumask_lock;
+
+/* Go to max speed when CPU load at or above this value. */
+#define DEFAULT_GO_MAXSPEED_LOAD 85
+static unsigned long go_maxspeed_load;
 
 /*
  * The minimum amount of time to spend at a frequency before we can ramp down.
  */
 #define DEFAULT_MIN_SAMPLE_TIME 80000;
 static unsigned long min_sample_time;
-
-#define LOAD_SCALE_MAX 85
 
 #define DEBUG 0
 #define BUFSZ 128
@@ -240,7 +244,7 @@ static void cpufreq_interactive_timer(unsigned long data)
 	if (load_since_change > cpu_load)
 		cpu_load = load_since_change;
 
-	if (cpu_load >= LOAD_SCALE_MAX)
+	if (cpu_load >= go_maxspeed_load)
 		new_freq = pcpu->policy->max;
 	else
 		new_freq = pcpu->policy->max * cpu_load / 100;
@@ -276,14 +280,18 @@ static void cpufreq_interactive_timer(unsigned long data)
 
 	if (new_freq < pcpu->target_freq) {
 		pcpu->target_freq = new_freq;
+		spin_lock(&down_cpumask_lock);
 		cpumask_set_cpu(data, &down_cpumask);
+		spin_unlock(&down_cpumask_lock);
 		queue_work(down_wq, &freq_scale_down_work);
 	} else {
 		pcpu->target_freq = new_freq;
 #if DEBUG
 		up_request_time = ktime_to_us(ktime_get());
 #endif
+		spin_lock(&up_cpumask_lock);
 		cpumask_set_cpu(data, &up_cpumask);
+		spin_unlock(&up_cpumask_lock);
 		wake_up_process(up_task);
 	}
 
@@ -425,14 +433,20 @@ static int cpufreq_interactive_up_task(void *data)
 
 	while (1) {
 		set_current_state(TASK_INTERRUPTIBLE);
+		spin_lock(&up_cpumask_lock);
 
-		if (cpumask_empty(&up_cpumask))
+		if (cpumask_empty(&up_cpumask)) {
+			spin_unlock(&up_cpumask_lock);
 			schedule();
+
+			if (kthread_should_stop())
+				break;
+
+			spin_lock(&up_cpumask_lock);
+		}
 
 		set_current_state(TASK_RUNNING);
 
-		if (kthread_should_stop())
-			break;
 #if DEBUG
 		then = up_request_time;
 		now = ktime_to_us(ktime_get());
@@ -446,9 +460,10 @@ static int cpufreq_interactive_up_task(void *data)
 #endif
 
 		tmp_mask = up_cpumask;
+		cpumask_clear(&up_cpumask);
+		spin_unlock(&up_cpumask_lock);
 
 		for_each_cpu(cpu, &tmp_mask) {
-			cpumask_clear_cpu(cpu, &up_cpumask);
 			pcpu = &per_cpu(cpuinfo, cpu);
 
 			if (nr_running() == 1) {
@@ -472,23 +487,40 @@ static int cpufreq_interactive_up_task(void *data)
 static void cpufreq_interactive_freq_down(struct work_struct *work)
 {
 	unsigned int cpu;
-	cpumask_t tmp_mask = down_cpumask;
+	cpumask_t tmp_mask;
 	struct cpufreq_interactive_cpuinfo *pcpu;
 
-	for_each_cpu(cpu, &tmp_mask) {
-		cpumask_clear_cpu(cpu, &down_cpumask);
-		pcpu = &per_cpu(cpuinfo, cpu);
+	spin_lock(&down_cpumask_lock);
+	tmp_mask = down_cpumask;
+	cpumask_clear(&down_cpumask);
+	spin_unlock(&down_cpumask_lock);
 
+	for_each_cpu(cpu, &tmp_mask) {
+		pcpu = &per_cpu(cpuinfo, cpu);
 		__cpufreq_driver_target(pcpu->policy,
 					pcpu->target_freq,
 					CPUFREQ_RELATION_H);
-
 		pcpu->freq_change_time_in_idle =
 			get_cpu_idle_time_us(cpu,
 					     &pcpu->freq_change_time);
 		dbgpr("down %d: set tgt=%d (actual=%d)\n", cpu, pcpu->target_freq, pcpu->policy->cur);
 	}
 }
+
+static ssize_t show_go_maxspeed_load(struct kobject *kobj,
+				     struct attribute *attr, char *buf)
+{
+	return sprintf(buf, "%lu\n", go_maxspeed_load);
+}
+
+static ssize_t store_go_maxspeed_load(struct kobject *kobj,
+			struct attribute *attr, const char *buf, size_t count)
+{
+	return strict_strtoul(buf, 0, &go_maxspeed_load);
+}
+
+static struct global_attr go_maxspeed_load_attr = __ATTR(go_maxspeed_load, 0644,
+		show_go_maxspeed_load, store_go_maxspeed_load);
 
 static ssize_t show_min_sample_time(struct kobject *kobj,
 				struct attribute *attr, char *buf)
@@ -506,6 +538,7 @@ static struct global_attr min_sample_time_attr = __ATTR(min_sample_time, 0644,
 		show_min_sample_time, store_min_sample_time);
 
 static struct attribute *interactive_attributes[] = {
+	&go_maxspeed_load_attr.attr,
 	&min_sample_time_attr.attr,
 	NULL,
 };
@@ -581,6 +614,7 @@ static int __init cpufreq_interactive_init(void)
 	struct cpufreq_interactive_cpuinfo *pcpu;
 	struct sched_param param = { .sched_priority = MAX_RT_PRIO-1 };
 
+	go_maxspeed_load = DEFAULT_GO_MAXSPEED_LOAD;
 	min_sample_time = DEFAULT_MIN_SAMPLE_TIME;
 
 	/* Initalize per-cpu timers */
@@ -608,6 +642,9 @@ static int __init cpufreq_interactive_init(void)
 
 	INIT_WORK(&freq_scale_down_work,
 		  cpufreq_interactive_freq_down);
+
+	spin_lock_init(&up_cpumask_lock);
+	spin_lock_init(&down_cpumask_lock);
 
 #if DEBUG
 	spin_lock_init(&dbgpr_lock);
