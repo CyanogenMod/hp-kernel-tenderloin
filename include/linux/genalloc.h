aadfd8715946ec89720d0abc5083fe23b733353e
Michal Nazarewicz <mina86@mina86.com>
2010-12-15 21:34:23 +0100

    lib: add gen_pool_alloc_aligned()
    This commit adds a gen_pool_alloc_aligned() function to the
    generic allocator API.  It allows specifying alignment for the
    allocated block.  This feature uses
    the bitmap_find_next_zero_area_off() function.
    
    It also fixes possible issue with bitmap's last element being
    not fully allocated (ie. space allocated for chunk->bits is
    not a multiple of sizeof(long)).
    
    It also makes some other smaller changes:
    - moves structure definitions out of the header file,
    - adds __must_check to functions returning value,
    - makes gen_pool_add() return -ENOMEM rater than -1 on error,
    - changes list_for_each to list_for_each_entry, and
    - makes use of bitmap_clear().
    
    Change-Id: Ib59f54d51ffe4c4fb747defc97a6581c46565890
    Signed-off-by: Michal Nazarewicz <mina86@mina86.com>
    Signed-off-by: Larry Bassel <lbassel@codeaurora.org>

--- include/linux/genalloc.h
+++ include/linux/genalloc.h
@@ -8,29 +8,31 @@
  * Version 2.  See the file COPYING for more details.
  */
 
+struct gen_pool;
 
-/*
- *  General purpose special memory pool descriptor.
+struct gen_pool *__must_check gen_pool_create(unsigned order, int nid);
+
+int __must_check gen_pool_add(struct gen_pool *pool, unsigned long addr,
+			      size_t size, int nid);
+
+void gen_pool_destroy(struct gen_pool *pool);
+
+unsigned long __must_check
+gen_pool_alloc_aligned(struct gen_pool *pool, size_t size,
+		       unsigned alignment_order);
+
+/**
+ * gen_pool_alloc() - allocate special memory from the pool
+ * @pool:	Pool to allocate from.
+ * @size:	Number of bytes to allocate from the pool.
+ *
+ * Allocate the requested number of bytes from the specified pool.
+ * Uses a first-fit algorithm.
  */
-struct gen_pool {
-	rwlock_t lock;
-	struct list_head chunks;	/* list of chunks in this pool */
-	int min_alloc_order;		/* minimum allocation order */
-};
+static inline unsigned long __must_check
+gen_pool_alloc(struct gen_pool *pool, size_t size)
+{
+	return gen_pool_alloc_aligned(pool, size, 0);
+}
 
-/*
- *  General purpose special memory pool chunk descriptor.
- */
-struct gen_pool_chunk {
-	spinlock_t lock;
-	struct list_head next_chunk;	/* next chunk in pool */
-	unsigned long start_addr;	/* starting address of memory chunk */
-	unsigned long end_addr;		/* ending address of memory chunk */
-	unsigned long bits[0];		/* bitmap for allocating memory chunk */
-};
-
-extern struct gen_pool *gen_pool_create(int, int);
-extern int gen_pool_add(struct gen_pool *, unsigned long, size_t, int);
-extern void gen_pool_destroy(struct gen_pool *);
-extern unsigned long gen_pool_alloc(struct gen_pool *, size_t);
-extern void gen_pool_free(struct gen_pool *, unsigned long, size_t);
+void gen_pool_free(struct gen_pool *pool, unsigned long addr, size_t size);
