e6fe882315d3e57d4302f09a847e82e3e7c7b3c6
James Sullins <jcsullins@gmail.com>
2012-02-27 17:53:32 -0600

    klog: add support for /proc/last_klog
    Change-Id: I523303b0cf40dea1269c08db2bac30ac83233566

--- drivers/misc/klog.c
+++ drivers/misc/klog.c
@@ -1,6 +1,7 @@
 /*
  *
  * Copyright (C) 2008-2009 Palm, Inc.
+ * Copyright (C) 2012 James Sullins
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -33,12 +34,24 @@
 #include <asm/memory.h>
 #include <asm/io.h>
 
+#include <linux/platform_device.h>
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
+#include <linux/io.h>
+
 #define MIN(a,b) ((a)<(b) ? (a):(b))
 
 #define KLOG_MAGIC 0x6b6c6f67 // 'klog'
 #define KLOG_VERSION 1
 
 extern int log_buf_get_len(void);
+
+static const char *last_klog_names[] = {
+	"last_klog",
+	"last_klog2",
+	"last_klog3",
+	NULL
+};
 
 struct klog_header {
 	uint32_t magic;
@@ -80,6 +93,39 @@ static DEFINE_SPINLOCK(klog_lock);
 static inline struct klog_buffer_header *get_kbuf(int num)
 {
 	return (struct klog_buffer_header *)((uint8_t *)klog + klog->buf_table[num]);
+}
+
+static inline struct klog_buffer_header *get_last_kbuf(int num)
+{
+	int lastnum;
+
+	if (num < 1 || num >= klog->buf_count) return NULL;
+
+	lastnum = klog->current_buf - num;
+	if (lastnum <0) lastnum += klog->buf_count;
+
+	return (struct klog_buffer_header *)
+		((uint8_t *)klog + klog->buf_table[lastnum]);
+}
+
+static inline unsigned get_last_klog_size(int num)
+{
+	struct klog_buffer_header *lkbuf = get_last_kbuf(num);
+
+	if (!lkbuf) return 0;
+
+	if (lkbuf->magic != KLOG_BUFFER_MAGIC) {
+		return 0;
+	}
+	else if (lkbuf->tail == lkbuf->head) {
+		return 0;
+	}
+	else if (lkbuf->head > lkbuf->tail) {
+		return lkbuf->head - lkbuf->tail;
+	}
+	else {
+		return lkbuf->len - 1;
+	}
 }
 
 static inline uint32_t
@@ -187,13 +233,104 @@ void klog_write(const char *s, unsigned int count)
 	spin_unlock_irqrestore(&klog_lock, flags);
 }
 
+static ssize_t last_klog_read(struct file *file, char __user *buf,
+				    size_t len, loff_t *offset)
+{
+	loff_t pos = *offset;
+	ssize_t count, lcount, ocount;
+	unsigned lastnum = 0;
+	unsigned lastsize;
+	const char* req_name = NULL;
+	struct klog_buffer_header *lkbuf;
+	char *lbuf;
+	unsigned i;
+
+	if (file && file->f_path.dentry) {
+		req_name = file->f_path.dentry->d_name.name;
+	}
+
+	if (!req_name) return -EFAULT;
+
+	for (i = 0; (i < klog->buf_count - 1 && last_klog_names[i]); i++) {
+		if (!strcmp(req_name, last_klog_names[i])) {
+			lastnum = i + 1;
+			break;
+		}
+	}
+	if (lastnum == 0) return -EFAULT;
+
+	lastsize = get_last_klog_size(lastnum);
+	if (pos >= lastsize) return 0;
+
+	lkbuf = get_last_kbuf(lastnum);
+	lbuf = (char*)lkbuf->data;
+
+	if (lkbuf->head > lkbuf->tail) {
+		count = min(len, (size_t)(lastsize - pos));
+		if (copy_to_user(buf, lbuf + lkbuf->tail + pos, count))
+			return -EFAULT;
+
+		*offset += count;
+		return count;
+	}
+
+	count = min(len, (size_t)(lastsize - pos));
+	ocount = count;
+
+	while (count) {
+		if (pos >= 0 && pos < lkbuf->len - lkbuf->tail) {
+			lcount = min((uint32_t)count, (uint32_t)(lkbuf->len - lkbuf->tail)
+							- (uint32_t)pos);
+			if (copy_to_user(buf, lbuf + lkbuf->tail + pos, lcount))
+				return -EFAULT;
+		} else {
+			lcount = min((uint32_t)count,
+							lastsize - (lkbuf->len - lkbuf->tail));
+			if (copy_to_user(buf, lbuf + pos - (lkbuf->len - lkbuf->tail),
+								lcount))
+				return -EFAULT;
+		}
+		buf += lcount;
+		pos += lcount;
+		count -= lcount;
+	}
+
+	*offset += ocount;
+	return ocount;
+}
+
+static const struct file_operations last_klog_file_ops = {
+	.owner = THIS_MODULE,
+	.read = last_klog_read,
+};
+
+void setup_proc_last_klog(void)
+{
+	struct proc_dir_entry *entry;
+	unsigned i;
+
+	for (i = 0; (i < klog->buf_count - 1 && last_klog_names[i]); i++) {
+		entry = create_proc_entry(last_klog_names[i]
+									, S_IFREG | S_IRUGO, NULL);
+		if (!entry) {
+			printk(KERN_ERR "klog_init: failed to create proc entry '%s'\n",
+					last_klog_names[i]);
+		} else {
+			entry->proc_fops = &last_klog_file_ops;
+			entry->size = get_last_klog_size(i+1);
+			printk(KERN_INFO "klog_init: %s head=%u tail=%u size=%u\n",
+					last_klog_names[i], get_last_kbuf(i+1)->head,
+					get_last_kbuf(i+1)->tail, get_last_klog_size(i+1));
+		}
+	}
+}
+
 static int __init klog_init(void)
 {
 	void *base;
 	unsigned long flags;
 
-	printk("klog_init: entry\n");
-	printk("klog_init: phys buffer is at 0x%lx\n", klog_phys);
+	printk(KERN_INFO "klog_init: phys buffer at 0x%lx\n", klog_phys);
 
 	if (klog_phys == 0 || klog_len == 0)
 	    return 0;
@@ -208,17 +345,15 @@ static int __init klog_init(void)
 	/* set up the klog structure */
 	klog_buffer = (char *)base;
 	klog = (struct klog_header *)klog_buffer;
-	printk("klog_init: virt address is %p\n", klog);
-
-	printk("klog_init: magic 0x%x version 0x%x\n", klog->magic, klog->ver);
+	printk(KERN_INFO "klog_init: virt address at 0x%p\n", klog);
 
 	/* check to see if it's valid */
 	if (klog->magic != KLOG_MAGIC || klog->ver != KLOG_VERSION) {
-	    printk("klog_init: didn't find existing klog\n");
+	    printk(KERN_ERR "klog_init: valid klog not found\n");
 	    return 0;
 	}
 
-	printk("found klog, len %u, using buffer number %d\n", klog->len, klog->current_buf);
+	printk(KERN_INFO "klog_init: found valid klog, len %u\n", klog->len);
 
 	klog_buf = get_kbuf(klog->current_buf);
 
@@ -230,7 +365,10 @@ static int __init klog_init(void)
 
 	spin_unlock_irqrestore(&klog_lock, flags);
 
-	printk(KERN_INFO "welcome to klog, buffer at %p, length %d\n", klog_buf, klog_buf->len);
+	printk(KERN_INFO "klog_init: using buffer %u at 0x%p, length %d\n",
+			klog->current_buf, klog_buf, klog_buf->len);
+
+	setup_proc_last_klog();
 
 	return 0;
 }
