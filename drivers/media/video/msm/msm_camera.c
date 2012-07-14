5764854467ca8a13f044fe5685bb2e591bb783ae
Tomasz Rostanski <rostan@podkowa.net>
2012-07-14 20:15:14 +0200

    msm: camera: Handle event queue overflow error
    This is port of:
    https://www.codeaurora.org/gitweb/quic/la/?p=kernel/msm.git;a=patch;h=bdf7b93f77e67974f5c6a2b6d91b9818e40cbb36

--- drivers/media/video/msm/msm_camera.c
+++ drivers/media/video/msm/msm_camera.c
@@ -2926,6 +2926,12 @@ static void msm_vpe_sync(struct msm_vpe_resp *vdata,
 	CDBG("%s: msm_enqueue event_q\n", __func__);
 	if (sync->frame_q.len <= 100 && sync->event_q.len <= 100) {
 		msm_enqueue(&sync->event_q, &qcmd->list_config);
+	} else if (sync->event_q.len > 100) {
+		pr_err("%s, Error Event Queue limit exceeded f_q = %d, e_q = %d\n",
+			__func__, sync->frame_q.len, sync->event_q.len);
+		qcmd->error_code = 0xffffffff;
+		qcmd->command = NULL;
+		msm_enqueue(&sync->frame_q, &qcmd->list_frame);
 	} else {
 		pr_err("%s, Error Queue limit exceeded f_q = %d, e_q = %d\n",
 			__func__, sync->frame_q.len, sync->event_q.len);
